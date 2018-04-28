77afd6ad7a4b540671cb8ea088f47010269c75c3
Collier <cole.hansen@gmail.com>
2018-04-28 11:25:01 -0700

    Twitter is working and Spotify is on the way

--- liri.js
+++ liri.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
+var keys = require("./keys.js");
+var Twitter = require('twitter');
+var nodeSpotifyAPI = require('node-spotify-api');
+var request = require('request');
 
-var spotify = new Spotify(keys.spotify);
+var spotify = new nodeSpotifyAPI(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var userInput = process.argv;
 var action = userInput[2];
 var query = userInput[3];
+var lineBreak = "---------------------------";
 
 switch (action) {
     case 'my-tweets':
@@ -26,10 +31,31 @@ switch (action) {
 
 function myTweets() {
     // This will be where I respond to my-tweets action
+    var params = { screen_name: 'groupBRCA' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            // console.log(tweets);
+        }
+        for (var i = 0; i < tweets.length; i++) {
+            var data = tweets[i];
+            console.log("User: " + data.user.name);
+            console.log("Tweet: " + data.text);
+            console.log("Created at: " + data.created_at);
+            console.log(lineBreak);
+        }
+    });
 }
 
 function spotifyThisSong() {
     // This is where I will repond to spotify-this-song
+    spotify
+        .search({ type: 'track', query: query, limit: 3 })
+        .then(function (response) {
+            console.log(response.tracks.items);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 
 function movieThis() {
